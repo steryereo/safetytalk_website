86a22d3cffd760fb261a7dedb7e8d1910cee0455
Nick Echols <stere.ereo@gmail.com>
2014-10-16 19:23:42 -0700

    embed height

--- music.php
+++ music.php
@@ -15,7 +15,7 @@ include('_head.php')
     <!-- BEGIN CONTENT -->
     <div id='row1'>
         <section class='content'>
-            <iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/51941164&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>    
+            <iframe width="100%" height="600" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/51941164&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>    
           
         </section>
         </div>
