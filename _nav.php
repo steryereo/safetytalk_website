46aa052d368f79110bb6dc642451577f0a95655f
Nick Echols <stere.ereo@gmail.com>
2014-10-19 01:48:32 -0700

    cleanup

--- _nav.php
+++ _nav.php
@@ -2,7 +2,7 @@
     	<div id = "nav-bg"></div>
             <ul>
                 <li id='logo-item'><a href='./'><img src='img/logo.png'></a></li>
-                <li><a href='./bio'>bio</a></li>
+                <li ><a href='./bio'>bio</a></li>
                 <li><a href='./photos'>photos</a></li>
                 <li><a href='./music'>music</a></li>
                 <li><a href='./'>booking</a></li>
