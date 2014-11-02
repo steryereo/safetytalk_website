25c1d9208281d5923e97d10d0926af46ae73056f
Nick Echols <stere.ereo@gmail.com>
2014-11-01 19:43:23 -0700

    logo with words

--- _nav.php
+++ _nav.php
@@ -1,7 +1,7 @@
     <div id='h-menu' >
     	<div id = "nav-bg"></div>
             <ul>
-                <li id='logo-item'><a href='./'><img src='img/logo.png'></a></li>
+                <li id='logo-item'><a href='./'><img src='img/logo_words.png'></a></li>
                 <li ><a href='./bio'>bio</a></li>
                 <li><a href='./photos'>photos</a></li>
                 <li><a href='./music'>music</a></li>
