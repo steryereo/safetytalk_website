bdfd6c2576716b99904096d3a2e01ea04d8e5171
Nick Echols <stere.ereo@gmail.com>
2014-11-25 12:50:07 -0800

    gifctrl

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 
     var coverphotos = ["<img id='animation' src='img/st_sequence_slower.gif'> \
-                    <div id='gifctrl' style='display:none;'> \
+                    <div id='gifctrl' > \
                         <input type='checkbox' id='playstop' > \
                         <label for='playstop'><span></span></label> \
                     </div>", 
@@ -10,18 +10,24 @@ $(document).ready(function() {
                     "<img id='lizard' src='img/2014-11-22_lizard.jpg'> \
                     <h6>11/22/2014 by Dave</h6>",
                 ];
+
+
+var gifctrl = function() {
+	$('#playstop').prop('checked', true);
+  	$('#playstop').change(function() {
+  		var s = $('#playstop').is(':checked') ? 'img/st_sequence_slower.gif' : 'img/safety-talk-composite.jpg';
+  		$('#animation').attr('src', s);
+	});
+  };
+
     var last = coverphotos.length-1;
     var i = last;
     $('#coverphoto').html(coverphotos[last]);
     $('#next').click(function () {
         if (i < 1) i = coverphotos.length;
         $('#coverphoto').html(coverphotos[--i]);
+        if (i === 0) gifctrl(); 
     });
 
 
-	$('#playstop').prop('checked', true);
-  	$('#playstop').change(function() {
-  		var s = $('#playstop').is(':checked') ? 'img/st_sequence_slower.gif' : 'img/safety-talk-composite.jpg';
-  		$('#animation').attr('src', s);
-	})
 });
