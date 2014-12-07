7c0dcc661eb23c2cd78ba3797de6485243358036
Nick Echols <stere.ereo@gmail.com>
2014-12-06 17:22:41 -0800

    alt text for logo

--- _nav.php
+++ _nav.php
@@ -2,7 +2,7 @@
     <div id='h-menu' >
     	<div id = "nav-bg"></div>
             <ul>
-                <li id='logo-item'><a href='./'><img src='img/logo_words.png'></a></li>
+                <li id='logo-item'><a href='./'><img src='img/logo_words.png' alt="Safety Talk. Psychedelic Country Surf from Oalkand, California."></a></li>
                 <!-- <li ><a href='./bio'>bio</a></li> -->
                 <li><a id="music-link" href='./music'>music</a></li>
                 <li><a id="photos-link" href='./photos'>photos</a></li>
