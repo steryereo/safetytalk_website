f3398ccce5bfdb9dff2d63bc91109dd458a98091
Nick Echols <stere.ereo@gmail.com>
2014-12-10 11:54:58 -0800

    blog progress

--- blog.php
+++ blog.php
@@ -15,17 +15,17 @@
     require_once('vendor/autoload.php');
 
 
-use Facebook\FacebookSession;
-//use Facebook\FacebookRedirectLoginHelper;
-use Facebook\FacebookRequest;
-use Facebook\FacebookResponse;
-use Facebook\FacebookSDKException;
-use Facebook\FacebookRequestException;
-use Facebook\FacebookAuthorizationException;
-use Facebook\GraphObject;
-use Facebook\Entities\AccessToken;
-use Facebook\HttpClients\FacebookCurlHttpClient;
-use Facebook\HttpClients\FacebookHttpable;
+    use Facebook\FacebookSession;
+    //use Facebook\FacebookRedirectLoginHelper;
+    use Facebook\FacebookRequest;
+    use Facebook\FacebookResponse;
+    use Facebook\FacebookSDKException;
+    use Facebook\FacebookRequestException;
+    use Facebook\FacebookAuthorizationException;
+    use Facebook\GraphObject;
+    use Facebook\Entities\AccessToken;
+    use Facebook\HttpClients\FacebookCurlHttpClient;
+    use Facebook\HttpClients\FacebookHttpable;
 
     FacebookSession::setDefaultApplication('833602023371005', '41e1ccf2c920aba7ca18a7b7c5224a66');
     $session = new FacebookSession('833602023371005|Wat1iKW1QXWjyfFCw9q7gr0W2fM');
@@ -45,7 +45,7 @@ use Facebook\HttpClients\FacebookHttpable;
         $graphObject = $response->getGraphObject();
     }
     
-?>
+    ?>
 
         <?php
             include ('_nav.php');
@@ -53,81 +53,90 @@ use Facebook\HttpClients\FacebookHttpable;
         //echo print_r($graphObject->getPropertyNames());
         ?>
         <div id='row1'>
-            <div class="content">
-            <div class="contentsection">
+        <div class="content">
+        <!-- <div class="contentsection"> -->
 
-                 <?php
+        <?php
             
-            echo "<div class=\"fb-feed\">";
+            // echo "<div class=\"fb-feed\">";
             
             // set counter to 0, because we only want to display 10 posts
             $i = 0;
             $posts = $graphObject->asArray();
                    
             foreach($posts['data'] as $post) {
-                
                 $type = $post->type;
-                 if ($type == 'status' || $type == 'link' || $type == 'photo'|| $type == 'video') {
-                    //echo "<code>".print_r($post)."</code><br><br>";
-                    
-                    // open up an fb-update div
-                    echo "<div class=\"contentsection\">";
-                        echo "post type: ".$type."<br>";
-                        // post the time
-                        //echo $post->created_time;
-                        
-                        // check if post type is a status
-                        if ($type == 'status') {
-                            echo "<p>" . $post->message . "</p>";
-                            echo "<p>".date("M jS, Y", (strtotime($post->created_time)))."</p>";
-                        }
-                        
-                        // check if post type is a link
-                        if ($type == 'link') {
-                            if (empty($post->picture) === false) {
-                               echo "<p><a href='".$post->link."' target='_blank'><img src='" . $post->picture . "'></a></p>";
-                            }
-                            echo "<p>" . $post->name . "</p>";
-                            echo "<p>" . $post->message . "</p>";
-                            echo "<p><a href=\"" . $post->link . "\" target=\"_blank\">" . $post->link . "</a></p>";
-                            echo "<p>".date("M jS, Y", (strtotime($post->created_time)))."</p>";
-                        }
-                        
+                //echo "<code>".print_r($post)."</code><br><br>";
+                $link = $post->link;
+
+                $source = $post->source;
+
+                $has_message = !empty($post->message); 
+                $has_picture = !empty($post->picture);
+                $has_youtube = strpos($link, "youtube") != FALSE || strpos($link, "youtu.be") != FALSE;
+                $has_soundcloud = strpos($link, "soundcloud") != FALSE;
+
+                // echo "has message:".print_r($has_message);
+                // echo "message:" .$post->message."<br>";
+
+                // echo "has picture:".$has_picture."<br>";
+                // echo "has youtube:".$has_youtube."<br>";
+                // echo "has soundcloud:".$has_soundcloud."<br>";
+                
+                // check if post type is a status
+                if ($has_message || $has_soundcloud || $has_youtube || $has_picture)  {
+                    echo "<div class='contentsection clearfix'>";
+                    if ($has_youtube){
+                        $dont_post = FALSE;
+                        parse_str( parse_url( $link, PHP_URL_QUERY ), $my_array_of_vars );
+                        $youtube_id = $my_array_of_vars['v'];    
+                        //echo $post->message;
+                        echo "<div class ='post-media'><iframe width='200' height='110' src='http://www.youtube.com/embed/". $youtube_id. "' frameborder='0' ></iframe></div>";
+                    }
+                    elseif ($has_picture) {
+                        echo "<div class ='post-media'><a href='".$post->link."' target='_blank'><img src='" . $post->picture . "'></a></div>";
+                    }
+                    elseif($has_soundcloud){
+                        preg_match("/url=(.*)&/", $source, $source_stripped);
+                        echo "".$post->message."";
+                        echo "<iframe width='100%' height='166' scrolling='no' frameborder='no' src='https://w.soundcloud.com/player/?url=". $source_stripped[1]."' ></iframe>";
+                    }
+                    echo "<div class='post-content'>";
+                    if ($has_message) {
+                        echo "<p>" . $post->message . "</p>";
+                        //echo "<p><a href=\"" . $post->link . "\" target=\"_blank\">" . $post->link . "</a></p>";
+                    }
+                    echo "<span class='post-date'>".date("M jS, Y", (strtotime($post->created_time)))."</span>";
+                    echo "</div></div>\n"; //contentsection
+                }
+                
                         // check if post type is a photo
-                        if ($type == 'photo') {
-                            echo "<p>" . $post->message . "</p>";
-                            echo "<p><a href='".$post->link."' target='_blank'><img src='" . $post->picture . "'></a></p>";
-                            echo "<p>".date("M jS, Y", (strtotime($post->created_time)))."</p>";
-                        }
+                        // if ($type == 'photo') {
+                        //     echo "<p>" . $post->message . "</p>";
+                        //     echo "<p><a href='".$post->link."' target='_blank'><img src='" . $post->picture . "'></a></p>";
+                        //     echo "<p>".date("M jS, Y", (strtotime($post->created_time)))."</p>";
+                        // }
 //<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/99714677&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
                         if ($type == 'video') {
-                            $youtube = $post->link;
-                            $source = $post->source;
-                            $embed = "embed/";
-                            if (strpos($youtube, "http://www.youtube.com/") === 0){
-                                $youtube = substr_replace($youtube, $embed, 23 , 0 );
-                                echo $post->message;
-                                echo "iframe width='200' height='110' src='". $youtube."' frameborder='0' allowfullscreen /iframe";
-                            }
-                            elseif(strpos($youtube, "http://youtu.be/") === 0){
-                                $youtube = str_replace("http://youtu.be/","http://www.youtube.com/", $youtube);
-                                $youtube = substr_replace($youtube, $embed, 23 , 0 );
-                                echo "".$post->message."";
-                                echo "iframe width='200' height='110' src='". $youtube."' frameborder='0' allowfullscreen /iframe";
-                            }
-                            elseif(strpos($source, "soundcloud") === 0){
-                                echo "".$post->message."";
-                                echo "<iframe> width='100%' height='166' scrolling='no' frameborder='no' src='". $source."' </iframe>";
-                            }else{
-                                echo "Video posted on: " . date("jS M, Y", (strtotime($post->created_time))) . "";
-                                echo "".$post->message."";
-                            }
+                            
+                            // $embed = "embed/";
+                            // if (strpos($link, "youtube") != FALSE){
+                            //     parse_str( parse_url( $link, PHP_URL_QUERY ), $my_array_of_vars );
+                            //     $youtube_id = $my_array_of_vars['v'];    
+                            //     echo "id= ".$youtube_id;
+                            //     echo $post->message;
+                            //     echo "<iframe width='200' height='110' src='http://www.youtube.com/embed/". $youtube_id. "' frameborder='0' />";
+                            // }
+                            // else{
+                            //     echo "Video posted on: " . date("jS M, Y", (strtotime($post->created_time))) . "";
+                            //     echo "".$post->message."";
+                            // }
 
-                            echo "<p>".date("M jS, Y", (strtotime($post->created_time)))."</p>";
+                            // echo "<p>".date("M jS, Y", (strtotime($post->created_time)))."</p>";
                         }
                     
                     
-                    echo "</div>"; // close fb-update div
+                    //echo "</div>"; // close fb-update div
                     
                 //     $i++; // add 1 to the counter if our condition for $type is met
                 // }
@@ -136,10 +145,8 @@ use Facebook\HttpClients\FacebookHttpable;
                 // //  break out of the loop if counter has reached 10
                 // if ($i == 10) {
                 //     break;
-                }
+                // }
             } // end the foreach statement
-            
-            echo "</div>";
             
         ?>
 <!-- <div class="fb-like-box" data-href="https://www.facebook.com/safetytalkinfinity" data-width="500" data-height="600" data-colorscheme="light" data-show-faces="false" data-header="false" data-stream="true" data-show-border="true"></div>
@@ -150,8 +157,9 @@ use Facebook\HttpClients\FacebookHttpable;
                 <a href='http://safetytalk.bandcamp.com' target='blank'><img src='img/bandcamp.png' /></a>
             </div>
         </div>
-        </div>
     </div>
+
+
         
 
         <?php
