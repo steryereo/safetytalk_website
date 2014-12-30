43bd4e35546dc741b7f22d8d666ba1682630e3d9
Nick Echols <stere.ereo@gmail.com>
2014-12-29 22:10:54 -0800

    typo

--- _contentheader.php
+++ _contentheader.php
@@ -1,7 +1,7 @@
 <header class='content-header clearfix'>
 <div class='header-titles'>
             <div class="header-title"><h1><em>Safety Talk</em></h1></div>
-            <div class="header-subtitle"><h6><span>Psychedelic Country Surf</span><span>from Oalkand, California</span></h6></div>
+            <div class="header-subtitle"><h6><span>Psychedelic Country Surf</span><span>from Oakland, California</span></h6></div>
         </div>
               <div id='links'>
                 <a href='http://www.facebook.com/safetytalkinfinity' target='blank'><img src='img/blah.png' /></a>
